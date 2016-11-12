1b81e0384151d0c7fdd446dc8d2f657c70931a83
Dirk Holtwick <dirk.holtwick@gmail.com>
2016-11-12 23:24:06 +0100

    Formatting

--- IPDFCameraViewController/IPDFCameraViewController.h
+++ IPDFCameraViewController/IPDFCameraViewController.h
@@ -9,8 +9,7 @@
 
 #import <UIKit/UIKit.h>
 
-typedef NS_ENUM(NSInteger,IPDFCameraViewType)
-{
+typedef NS_ENUM(NSInteger,IPDFCameraViewType) {
     IPDFCameraViewTypeBlackAndWhite,
     IPDFCameraViewTypeNormal
 };
